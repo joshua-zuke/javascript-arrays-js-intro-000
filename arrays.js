1117d55232973244060c18bb314d3a2c811909d9
Joshua Zuke <zuke.joshua@gmail.com>
2021-03-03 18:35:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,7 +19,7 @@ function addElementToEndOfArray(array, element){
 }
 
 function accessElementInArray(array, index){
-  return (array[index])
+  return array[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array){
